13c16c7a769ebf713248513f0ed6ac7cf646478b
ProstoCheI <shishkinm17@gmail.com>
2024-07-07 14:50:41 +0500

    Add player name

--- Test.py
+++ Test.py
@@ -1,12 +1,14 @@
 from random import randint
 import time
+name1 = input('Введите имя первого игрока: ')
+name2 = input('Введите имя второго игрока: ')
 num1 = input('Введите число первого игрока: ')
 num2 = input('Введите число второго игрока: ')
 a = randint(1, 6)
 time.sleep(3)
 if num1 == a:
-    print('Победил первый игрок')
+    print('Победил', name1)
 elif num2 == a:
-    print('Победил второй игрок')
+    print('Победил', name2)
 else:
     print('Ничья')
