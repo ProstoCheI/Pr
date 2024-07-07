e90bbe9d122776d3c41f7abd410fdf05f6ba9907
ProstoCheI <shishkinm17@gmail.com>
2024-07-07 14:55:02 +0500

    Add new line

--- Test.py
+++ Test.py
@@ -4,7 +4,9 @@ num1 = input('Введите число первого игрока: ')
 num2 = input('Введите число второго игрока: ')
 a = randint(1, 6)
 time.sleep(3)
-if num1 == a:
+if num1 == num2:
+    print('Введите разные числа')
+elif num1 == a:
     print('Победил первый игрок')
 elif num2 == a:
     print('Победил второй игрок')
