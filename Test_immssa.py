7c14eb99f3de7fd2e2c537101643e3f05eb83467
immssa <safronova2461@gmail.com>
2024-07-08 17:20:23 +0500

    commentariy

--- Test_immssa.py
+++ Test_immssa.py
@@ -1,11 +1,9 @@
 from random import randint
-import time
 name1 = input('Введите имя первого игрока: ')
 name2 = input('Введите имя второго игрока: ')
 num1 = input('Введите число первого игрока: ')
 num2 = input('Введите число второго игрока: ')
 a = randint(1, 6)
-time.sleep(3)
 if num1 == num2:
     print('Введите разные числа')
 elif num1 == a:
